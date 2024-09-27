9494bef35aa732572d43f4268dacd3f6b688146f
lauraboza <laurabozam26@gmail.com>
2024-09-27 09:53:14 +0200

    problema 1

--- Enunciados01.R
+++ Enunciados01.R
@@ -1,1 +1,19 @@
-# Hola esto es una prueba
+## ENUNCIADO TAREA 1
+
+# PROBLEMA 1
+
+# Sea la tabla de decisión con 5 estados de la naturaleza y 4 alternativas de la siguiente 
+# forma:
+
+X = matrix(c(5,4,6,2,3,1,7,8,7,5,2,0,8,9,5,4,3,-1,9,10),nrow=4,ncol=5,byrow=TRUE)
+colnames(X)=c('e1','e2','e3','e4','e5')
+rownames(X)=c('d1','d2','d3','d4')
+X
+
+# Resolverla tanto para situación favorable como desfavorable, con cada uno de los 
+# criterios por separado
+
+
+
+
+
